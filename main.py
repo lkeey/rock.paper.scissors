e1589513ece5a8725d4dc1d3ffafae4649072567
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2024-07-10 15:47:29 +0300

    add ability to register

--- main.py
+++ main.py
@@ -17,16 +17,31 @@ from telegram.ext import (
 )
 from configparser import ConfigParser
 from random import choice
+import aiosqlite
 
 config = ConfigParser()
 config.read("config.ini")
 BOT_TOKEN = config["Telegram"]["tg_token"]
 
 #states
-CHOOSING, CHOOSE_ACTION, NAME, PHONE = range(3)
+CHOOSING, CHOOSE_ACTION, NAME, PHONE = range(4)
 
 # callbacks
-CANCEL, PLAY, REGISTER = range(3)
+CANCEL, PLAY, REGISTER, LEADER_BOARD = range(4)
+
+# Database initialization
+async def init_db():
+    async with aiosqlite.connect('bot.db') as db:
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY,
+                wins INTEGER,
+                name TEXT,
+                phone TEXT
+            )
+        ''')
+        await db.commit()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
@@ -35,6 +50,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
             InlineKeyboardButton("Play", callback_data=PLAY),
             InlineKeyboardButton("Register", callback_data=REGISTER),
         ],
+        [
+            InlineKeyboardButton("LeaderBoard", callback_data=LEADER_BOARD),
+        ]
     ]
 
     await update.message.reply_text(
@@ -87,20 +105,43 @@ async def choosing_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         )
         return CHOOSE_ACTION
     elif int(query.data) == REGISTER:
+        async with aiosqlite.connect('bot.db') as db:
+            async with db.execute('SELECT name FROM users WHERE user_id = ?', (update.effective_user.id,)) as cursor:
+                row = await cursor.fetchone()
+                if row:
+                    await query.edit_message_text('You are already registered with the name')
+                    await start(query, context)
+                    return ConversationHandler.END
+
         await query.edit_message_text(text="Let's start registration")
 
         await query.message.reply_text('Please, send me your name')
         return NAME
-    else:
-        await query.edit_message_text(text="Bye!")
+    elif int(query.data) == LEADER_BOARD:
+        await query.edit_message_text(text="There are all users:")
+        await get_users(query, context)
+        await start(query, context)
 
-        await start(update, context)
+        return ConversationHandler.END
+    else:
+        await query.edit_message_text(text="Bye...")
+        await start(query, context)
+
         return ConversationHandler.END
 
 async def save_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     name = update.effective_message.text
+    id = update.effective_user.id
+    
     await update.message.reply_text(f'Thank you for sharing your name: {name}')
     
+    async with aiosqlite.connect('bot.db') as db:  
+        await db.execute('''
+            INSERT INTO users (user_id, wins, name, phone) 
+            VALUES (?, ?, ?, ?)
+        ''', (id, 0, name, "none"))
+        await db.commit()
+
     contact_button = KeyboardButton(text="Share your phone number", request_contact=True)
     reply_markup = ReplyKeyboardMarkup([[contact_button]], one_time_keyboard=True, resize_keyboard=True)
 
@@ -109,12 +150,30 @@ async def save_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def save_phone(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     phone_number = update.message.contact.phone_number
+    id = update.effective_user.id
+
     await update.message.reply_text(f'Thank you for sharing your phone number: {phone_number}')
     
-    # TODO save to db
+    async with aiosqlite.connect('bot.db') as db:
+        await db.execute('''
+            UPDATE users
+            SET phone = ?
+            WHERE user_id = ?
+        ''', (phone_number, id))
+        await db.commit()
+
     await start(update, context)
+
     return ConversationHandler.END
     
+async def get_users(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    async with aiosqlite.connect('bot.db') as db:
+        async with db.execute('SELECT user_id, wins, name, phone FROM users') as cursor:
+            rows = await cursor.fetchall()
+            messages = '\n'.join([f"{row[0]}: ({row[1]}) - {row[2]} - {row[3]}" for row in rows])
+
+    await update.message.reply_text(messages)
+
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
     await update.message.reply_text(
@@ -125,7 +184,6 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     return ConversationHandler.END
 
 def main() -> None:
-
     application = Application.builder().token(BOT_TOKEN).build()
 
     conv_handler = ConversationHandler(
@@ -154,4 +212,6 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    # import asyncio
+    # asyncio.run(init_db())
     main()
