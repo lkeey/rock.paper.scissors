354c9591446258971860a8f4cde36b303f00b690
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2024-07-12 23:02:42 +0300

    add score

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ import asyncio
 from warnings import filterwarnings
 from telegram.warnings import PTBUserWarning
 
+# dont show per_message error
 filterwarnings(action="ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning)
 
 config = ConfigParser()
@@ -34,19 +35,19 @@ CHOOSING, CHOOSE_ACTION, NAME, PHONE = range(4)
 # callbacks
 CANCEL, PLAY, REGISTER, LEADER_BOARD = range(4)
 
-# Database initialization
+# database initialization
 async def init_db():
     async with aiosqlite.connect('bot.db') as db:
         await db.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 user_id INTEGER PRIMARY KEY,
                 wins INTEGER,
+                defeats INTEGER,        
                 name TEXT,
                 phone TEXT
             )
         ''')
         await db.commit()
-
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
@@ -71,6 +72,9 @@ async def check_winner(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     
     computer = choice(["Rock", "Paper", "Scissors"])
     user = update.effective_message.text
+    user_id = update.effective_user.id
+
+    is_winner = False
 
     keyboard = [
         [
@@ -83,8 +87,43 @@ async def check_winner(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
 
     if (user == "Paper" and computer == "Rock") or (user == "Scissors" and computer == "Paper") or (user == "Rock" and computer == "Scissors"):
         message_to_user = "You won!!"
+        is_winner = True
+
     elif (user == "Rock" and computer == "Paper") or (user == "Paper" and computer == "Scissors") or (user == "Scissors" and computer == "Rock"):
         message_to_user = "You lost!!"
+        is_winner = False
+
+    async with aiosqlite.connect('bot.db') as db:
+        async with db.execute('SELECT wins, defeats FROM users WHERE user_id = ?', (user_id,)) as cursor:
+            row = await cursor.fetchone()
+            if row:
+                # if user exists, increment their points
+                print("ROW -", row, user_id)
+                try:
+                    if is_winner:
+                        w = row[0] + 1 if row[0] != None else 1
+                        await db.execute('''
+                            UPDATE users
+                            SET wins = ?
+                            WHERE user_id = ?
+                        ''', (w, user_id))
+                    else:
+                        d = row[1] + 1 if row[1] != None else 1
+                        await db.execute('''
+                            UPDATE users
+                            SET defeats = ?
+                            WHERE user_id = ?
+                        ''', (d, user_id))
+                except Exception as e:
+                    print(e)
+            else:
+                # if user does not exist set 1 or 0 change on get name
+                await db.execute('''
+                    INSERT INTO users (user_id, wins, defeats) 
+                    VALUES (?, ?, ?)
+                ''', (user_id, 1 if is_winner else 0, 0 if is_winner else 1))
+            await db.commit()
+
 
     await update.message.reply_text(
         f"Computer - {computer}\n{message_to_user}",
@@ -113,14 +152,14 @@ async def choosing_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         async with aiosqlite.connect('bot.db') as db:
             async with db.execute('SELECT name FROM users WHERE user_id = ?', (update.effective_user.id,)) as cursor:
                 row = await cursor.fetchone()
-                if row:
+                if row[0] != None:
                     await query.edit_message_text('You are already registered with the name')
                     
                     return await start(query, context)
 
         await query.edit_message_text(text="Let's start registration")
-
         await query.message.reply_text('Please, send me your name')
+        
         return NAME
     elif int(query.data) == LEADER_BOARD:
         await query.edit_message_text(text="There are all users:")
@@ -128,7 +167,7 @@ async def choosing_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         
         return await start(query, context)
     else:
-        await query.edit_message_text(text="Bye...")
+        await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
         
         return await start(query, context)
 
@@ -139,11 +178,20 @@ async def save_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Thank you for sharing your name: {name}')
     
     async with aiosqlite.connect('bot.db') as db:  
-        await db.execute('''
-            INSERT INTO users (user_id, wins, name, phone) 
-            VALUES (?, ?, ?, ?)
-        ''', (id, 0, name, "none"))
-        await db.commit()
+        async with db.execute('SELECT name FROM users WHERE user_id = ?', (update.effective_user.id,)) as cursor:
+            row = await cursor.fetchone()
+            if row[0] != None:
+                await db.execute('''
+                    UPDATE users
+                    SET name = ?
+                    WHERE user_id = ?
+                ''', (name, id))
+            else:
+                await db.execute('''
+                    INSERT INTO users (user_id, name) 
+                    VALUES (?, ?)
+                ''', (id, name))
+            await db.commit()
 
     contact_button = KeyboardButton(text="Share your phone number", request_contact=True)
     reply_markup = ReplyKeyboardMarkup([[contact_button]], one_time_keyboard=True, resize_keyboard=True)
@@ -169,12 +217,14 @@ async def save_phone(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
     
 async def get_users(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     async with aiosqlite.connect('bot.db') as db:
-        async with db.execute('SELECT user_id, wins, name, phone FROM users') as cursor:
+        async with db.execute('SELECT user_id, wins, defeats, name, phone FROM users') as cursor:
             rows = await cursor.fetchall()
-            messages = '\n'.join([f"{row[0]}: ({row[1]}) - {row[2]} - {row[3]}" for row in rows])
+            messages = '\n'.join([f"{row[0]}: ({row[1]} - {row[2]}) - {row[3]} - {row[4]}" for row in rows])
 
-    await update.message.reply_text(messages)
-
+    if len(messages) != 0:
+        await update.message.reply_text("user_id: (wins - defeats) - name - phone")
+        await update.message.reply_text(messages)
+        
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
     await update.message.reply_text(
@@ -185,7 +235,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 def main() -> None:
     print("MAIN")
-    
+
     application = Application.builder().token(BOT_TOKEN).build()
 
     conv_handler = ConversationHandler(
@@ -204,6 +254,7 @@ def main() -> None:
                 MessageHandler(filters.CONTACT, save_phone)
             ],
         },
+        #always handles 
         fallbacks=[CommandHandler("cancel", cancel)],
         # per_message=True,
     )
